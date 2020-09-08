0e63080aa09743fba95d374195a74a32fa85f4f3
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2020-09-08 13:56:43 -0300

    Add files via upload

---
+++ js/traffic.js
@@ -0,0 +1,8 @@
+var lorenipsen2 = 0
+function lorenipsen(){
+    lorenipsen2 += 1
+    if (lorenipsen2 >= 8){
+        alert("Solicitações Expiradas")
+        window.location.href= "https://felipeschmitt.github.io/tecbiosbrazil/"
+    }
+}
